23dd0a5cf938a2250271aba8c97eddbcfdf63c7a
Prince Choudhary <princechoudhary784@gmail.com>
2025-02-23 08:34:23 +0530

    Runing react with parcel bundler

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDom from "react-dom/client"
+import ReactDOM from "react-dom/client"
 
 const heading1 = React.createElement("h1", {id: "title", style: {backgroundColor: "red"}}, "Heading1");
 const heading2 = React.createElement("h2", {id: "title2"}, "Heading2");
 const container = React.createElement("div", {id: "box"}, [heading1, heading2]);
 
-const root = ReactDom.createRoot(document.getElementById("root"))
-root.render(container)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
