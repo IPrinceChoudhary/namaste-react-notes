bb85ae4b45586bfbf39250c92ca88b0f06e11b6b
Prince Choudhary <princechoudhary784@gmail.com>
2025-03-01 09:47:11 +0530

    added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,17 +2,20 @@ import { restaurantList } from "../Config.js";
 import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
 
+const filterRestaurant = (searchText, restaurants) => {
+  if(!searchText){
+    return restaurants
+  }
+  const searchFilteredRestaurant = restaurants.filter((restaurant) =>
+    restaurant.info.name.toLowerCase().includes(searchText.trim().toLowerCase())
+  );
+  return searchFilteredRestaurant
+};
+
 const Body = () => {
-  // let searchText = "text";
-  //* hooks are also functions comes with some functionality
-
-  //* every time if we want our variables to be sync with UI(for eg. updating them) we should use state variable as react does not track the normal variables(for eg. we changed using functions)
-
-  // searchText is a local state variable
-  const [searchText, setSearchText] = useState("text"); // to create state variables and other is function provided by useState hook
-  //* this useState hook returns an array and first element of an array is the variable name
-
-  const [searchClicked, setSearchClicked] = useState("true")
+  const [searchText, setSearchText] = useState("");
+  const [allRestaurants] = useState(restaurantList); // stores original list
+  const [restaurant, setRestaurant] = useState(restaurantList); // stores filtered list
 
   return (
     <>
@@ -23,20 +26,29 @@ const Body = () => {
           placeholder="Search"
           value={searchText}
           onChange={(e) => {
-            setSearchText(e.target.value)
+            setSearchText(e.target.value);
+          }}
+          onKeyDown={(e)=>{
+            if(e.key === "Enter"){
+              const data = filterRestaurant(searchText, allRestaurants);
+              setRestaurant(data);
+            }
           }}
         />
-        <h2>{searchClicked}</h2>
-        <button onClick={()=>{
-          if(searchClicked === "true"){
-            setSearchClicked("false")
-          }else{
-            setSearchClicked("true")
-          }
-        }}>submit</button>
+        <button
+          className="search-btn"
+          // need to filter data
+          onClick={() => {
+            const data = filterRestaurant(searchText, allRestaurants);
+            // update the restaurants
+            setRestaurant(data);
+          }}
+        >
+          search
+        </button>
       </div>
       <div className="restaurant-list">
-        {restaurantList.map((res) => {
+        {restaurant.map((res) => {
           return <RestaurantCard restaurant={res} key={res.info.id} />;
         })}
       </div>
