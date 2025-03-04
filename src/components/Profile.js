65d75c2c378ef4e0c3391f6cb0bd2bdeff9d4abb
Prince Choudhary <princechoudhary784@gmail.com>
2025-03-04 12:58:43 +0530

    api with compDidMount, compDidUpdate, compWillMount, life cycle method and clean up function in useEffect hook

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Profile = ()=>{
   const [count, setCount] = useState(0)
   const [count2] = useState(2)
+
+  useEffect(()=>{
+    const id = setInterval(()=>{
+      console.log("react op")
+    },1000) // will call this setInterval after each second even we change our page and if we come again to this page where this effect is running it will run twice at the same time and so on
+
+    // cleanup function 
+    return ()=>{ // unmounting phase what we saw in class components
+      clearInterval(id)
+    }
+  },[])
   return(
     <div>
       <h2>Profile functional comp</h2>
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -1,37 +1,42 @@
 import React from "react"
 
 class Profile extends React.Component{
-  // how to use state variables
+
   constructor(props){
     super(props)
-      //create state
       this.state = {
-        count: 0,
-        count2: 2,
+        userInfo: {},
       };
-      console.log("constructor is called first")
   }
 
-  //* these are all called life cycle methods, like how the cycle will be of this class. Constructor---> render---> componentDidMount
+  async componentDidMount(){
+    const data = await fetch("https://api.github.com/users/IPrinceChoudhary");
+    const response = await data.json()
+    this.setState({
+      userInfo: response
+    })
+  }
 
-  componentDidMount(){ //* will be called after render just like useEffect in functional component
-    // api calls 
+  //* life cycle in class - Mounting--> Updating(if state is being changed due to componentDidMount then this will start)--> Unmounting
+
+  //* called after re-render and update cycle started.
+  componentDidUpdate(){
+    console.log("componentDidUpdate")
+  }
+
+  // will be called after update cycle, for e.g. left the page because the component is not there so it will unmount
+  componentWillUnmount(){
+
   }
 
   render(){
     console.log("render is called after constructor")
     return(
       <div>
-        {/* to get props */}
         <h2>Profile class comp Name - {this.props.name}</h2> 
-        <h3>{this.state.count}</h3>
-        <h3>{this.state.count2}</h3>
-        <button onClick={()=>{
-          this.setState({
-            count: 1,
-            count2: 3, //* will only modify this state, even we don't pass nothing to upper count
-          })
-        }}>count</button>
+        <p>userInfo</p>
+        <img src={this.state.userInfo?.avatar_url} alt="" />
+        <h3>{this.state.userInfo?.name}</h3>
       </div>
     )
   }
